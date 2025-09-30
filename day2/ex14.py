9b4d242faf33b5da0e9ead2bab1fa76c5812af35
rajkonkret <rajkonkret660@gmail.com>
2025-09-30 14:18:57 +0200

    [2 16] update ex_14

--- day2/ex14.py
+++ day2/ex14.py
@@ -1,8 +1,36 @@
 import pandas as pd
+from pandas.core.config_init import max_cols
 
-df = pd.DataFrame[{
+df = pd.DataFrame({
     "Imie": ['Jan', "Anna", 'Tomek'],
     "Wiek": [29, 24, 35],
     "Miasto": ['Warszawa', "Wrocław", "Legnica"]
-}]
+})
 
+# kontekst menadżer
+with pd.ExcelWriter("Tabela.xlsx", engine='xlsxwriter') as writer:
+    df.to_excel(writer, sheet_name="Osoby", startrow=1, header=False, index=False)
+
+    workbook = writer.book
+    worksheet = writer.sheets['Osoby']
+
+    header_format = workbook.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'valign': "vcenter",
+        'bg_color': '#D7E4BC',
+        'border': 1
+    })
+
+    # enumerate() - numeruje kolekcje
+    for col_num, value in enumerate(df.columns.values):
+        worksheet.write(0, col_num, value, header_format)
+
+    (max_row, max_col) = df.shape  # odczyt kształtu tabeli
+    # chr() - znak o kodzie ascii,  chr(65) to A
+    cell_range = f"A1:{chr(65 + max_col - 1)}{max_row + 1}"
+
+    worksheet.add_table(cell_range, {
+        'name': "TabelaOsoby",
+        "columns": [{"header": col} for col in df.columns]
+    })
