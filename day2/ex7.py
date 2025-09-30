48f960f59cf46a02f15b55177447ed1fa915fdae
rajkonkret <rajkonkret660@gmail.com>
2025-09-30 10:13:50 +0200

    [2 3] add ex7

---
+++ day2/ex7.py
@@ -0,0 +1,45 @@
+import openpyxl
+from openpyxl.chart import Reference, BarChart
+from openpyxl.chart.axis import ChartLines
+
+wb = openpyxl.load_workbook('video2.xlsx')
+ws = wb['Total Sales by Genre']
+
+# wycinamy dane z excela potrzebne do wykresu
+values = Reference(ws,
+                   min_col=2,
+                   max_col=2,
+                   min_row=1,
+                   max_row=13)
+
+cats = Reference(ws,
+                 min_col=1,
+                 max_col=1,
+                 min_row=2,
+                 max_row=13)
+
+# tworzenie wykresu, wykres słupkowy
+chart = BarChart()
+chart.add_data(values, titles_from_data=True)
+chart.set_categories(cats)
+
+chart.title = "Total Sales"
+chart.x_axis.title = "Genre"
+chart.y_axis.title = "Total Sales by Genre"
+# chart.y_axis.majorUnit = 50
+# chart.y_axis.crossAx = 500
+# chart.y_axis.majorTickMark = "out"
+# chart.y_axis.majorTickMark = "in"
+# umiesczenie wykresu w komórce D2
+chart.x_axis.delete = False
+chart.y_axis.delete = False
+chart.y_axis.majorGridlines = ChartLines()
+chart.y_axis.tickLblPos = "nextTo"
+chart.y_axis.number_format = '0'
+
+chart.x_axis.majorGridlines = ChartLines()
+
+ws.add_chart(chart, 'D2')
+
+wb.save('video2.xlsx')
+wb.close()
